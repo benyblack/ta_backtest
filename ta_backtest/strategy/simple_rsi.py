2af263a105b0139a91142c1574c83e9a0f0f7dcf
Behnam <benyblack@gmail.com>
2019-06-14 17:53:55 +0200

    Refactor simple rsi strategy to return trade list

--- ta_backtest/strategy/simple_rsi.py
+++ ta_backtest/strategy/simple_rsi.py
@@ -9,25 +9,28 @@ def can_sell(rsi_list: [float], index: int):
     return rsi_list[index] > 70
 
 
-def do_iterate(data: [], init_cash: float):
+def do_trades(data: [], init_cash: float) -> (float, {}):
     rsi_list = rsi.rsi(data)
     buy_points = [index for index, val in enumerate(data) if can_buy(rsi_list, index)]
     if len(buy_points) == 0:
         return init_cash
     sell_points = [index for index, val in enumerate(data) if can_sell(rsi_list, index)]
+    history = {}
 
-    def buy(buy_index, cash):
-        print(f'buy: {buy_index}: {data[buy_index]}')
+    def buy(buy_index, cash, old_history):
+        new_transaction = {buy_index: {'type': 'BUY', 'price': data[buy_index], 'cash': cash}}
+        new_history = {**old_history, **new_transaction}
         sell_indexes = [index for index, val in enumerate(sell_points) if index > buy_index]
         if len(sell_indexes) == 0:
-            return cash
-        return sell(sell_indexes[0], buy_index, cash)
+            return cash, new_history
+        return sell(sell_indexes[0], buy_index, cash, new_history)
 
-    def sell(sell_index, buy_index, cash):
-        print(f'sell: {sell_index}: {data[sell_index]}')
+    def sell(sell_index, buy_index, cash, old_history):
+        new_transaction = {buy_index: {'type': 'SELL', 'price': data[buy_index], 'cash': cash}}
+        new_history = {**old_history, **new_transaction}
         buy_indexes = [index for index, val in enumerate(buy_points) if index > sell_index]
         if len(buy_indexes) == 0:
-            return cash
-        return buy(buy_indexes[0], (data[sell_index]/data[buy_index])*cash)
+            return cash, new_history
+        return buy(buy_indexes[0], (data[sell_index]/data[buy_index])*cash, new_history)
 
-    print(buy(buy_points[0], init_cash))
+    return buy(buy_points[0], init_cash, history)
