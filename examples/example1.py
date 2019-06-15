1c622ce84e1a25fd7b386c8ddee5210b9e79c21b
Behnam <benyblack@gmail.com>
2019-06-15 22:30:39 +0200

    Add the first example

---
+++ examples/example1.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append("..")
+from ta_backtest.strategy import simple_rsi
+from ta_backtest.tools import data_loader as loader
+from ta_backtest.tools.rsi import rsi as rsi
+import ta_backtest.chart as chart
+
+
+data = loader.from_csv('./1h-BTCUSDT.csv')
+close_data = [x for x in data.Close]
+date_data = [x for x in data.DateTime]
+cash, trade_history = simple_rsi.do_trades(data.Close, 1)
+
+oscillator_data = [{'title': 'RSI 14', 'data': rsi(close_data)}]
+
+plot, oscillator, trades = chart.make_chart("BTC_USDT", close_data, date_data, oscillator_data, trade_history)
+chart.show_plot(plot, oscillator, trades)
--- ta_backtest/trade_runner.py
+++
@@ -1,6 +0,0 @@
-from strategy import simple_rsi as rsi
-from tools import data_loader as loader
-
-
-data = loader.from_csv('../../binance_data/data/BTCUSDT/4h-BTCUSDT.csv')
-rsi.do_iterate(data.Close, 1)
