2116c047abf89980ce8ce53b693d176d2baeab59
Behnam <benyblack@gmail.com>
2019-06-23 12:23:39 +0200

    Add sma crossover example and 1d btc data

--- examples/example1.py
+++ examples/example1.py
@@ -6,9 +6,11 @@ from ta_backtest.tools.rsi import rsi as rsi
 import ta_backtest.chart as chart
 import ta_backtest.strategy.percent_jump as percent
 import ta_backtest.strategy.trader as trader
+import ta_backtest.tools.sma as sma
+import ta_backtest.strategy.sma_crossover as sma_crossover
 
 
-data = loader.from_csv('./1h-BTCUSDT.csv')
+data = loader.from_csv('./1d-BTCUSDT.csv')
 close_data = [x for x in data.Close]
 date_data = [x for x in data.DateTime]
 cash = 1
@@ -20,7 +22,11 @@ commision = 0.001
 # cash, trade_history = trader.do_trades(data.Close, potential_trades, cash, commision)
 
 # 2. Percent jump
-potential_trades = percent.potential_trades(data.Close)
+# potential_trades = percent.potential_trades(data.Close)
+# cash, trade_history = trader.do_trades(data.Close, potential_trades, cash, commision)
+
+# 3. SMA cross-over
+potential_trades = sma_crossover.potential_trades(data.Close)
 cash, trade_history = trader.do_trades(data.Close, potential_trades, cash, commision)
 
 # Indicator(s)
@@ -28,4 +34,10 @@ oscillator_data = [{'title': 'RSI 14', 'data': rsi(close_data)}]
 
 # Make and show chart
 plot, oscillator, trades = chart.make_chart("BTC_USDT", close_data, date_data, oscillator_data, trade_history)
+
+# Add SMA 10, 20 for the third example
+plot = chart.add_line_to_plot(plot, "SMA 10", date_data, sma.sma(close_data, 10), "blue")
+plot = chart.add_line_to_plot(plot, "SMA 20", date_data, sma.sma(close_data, 20), "maroon")
+
+# Show the plot in the browser
 chart.show_plot(plot, oscillator, trades)
