0ca040d1964e579e6b74d5174f414536633bed6b
Behnam <benyblack@gmail.com>
2019-06-23 11:09:40 +0200

    Refactor simple_rsi, Add constants

--- ta_backtest/strategy/simple_rsi.py
+++ ta_backtest/strategy/simple_rsi.py
@@ -1,12 +1,15 @@
 import ta_backtest.tools.rsi as rsi
 
+RSI_BUY_LIMIT = 30
+RSI_SELL_LIMIT = 70
+
 
 def can_buy(rsi_list: [float], index: int):
-    return rsi_list[index] < 30
+    return rsi_list[index] < RSI_BUY_LIMIT
 
 
 def can_sell(rsi_list: [float], index: int):
-    return rsi_list[index] > 70
+    return rsi_list[index] > RSI_SELL_LIMIT
 
 
 def potential_trades(data):
